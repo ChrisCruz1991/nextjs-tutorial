7b2380fe52cefc8381b9c2734060d2f67a3cc400
Christopher Cruz <christopher.cruz@@upr.edu>
2024-02-25 16:34:17 -0400

    Implemented customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <p>Customers page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <Suspense fallback={<InvoicesTableSkeleton />}>
+      <Table customers={customers} />
+    </Suspense>
+  );
 }
